2df1ce5b94f48002b766cb8f0141a66b40c5e58a
Rithik Samanthula <talk2rithiks@gmail.com>
2021-05-21 15:55:47 -0400

    🔨 Build Complete

---
+++ build/precache-manifest.fd89b8af837016e60ca03978bb7c68d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f544fd9848b077837891a436044e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "a5b7785343803ae6c2f3",
+    "url": "/static/js/main.a0c423b3.chunk.js"
+  },
+  {
+    "revision": "164da21e09e43c8bb0c6",
+    "url": "/static/js/runtime-main.aa0ae0cd.js"
+  }
+]);
